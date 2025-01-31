16e1dcb9bb7dc51ebcae58873f99abec45e7cb3b
Christine <tinegulmatico@gmail.com>
2025-01-31 14:03:39 +0800

    added env

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Login endpoint
 export const startLogin = async (phone: string) => {

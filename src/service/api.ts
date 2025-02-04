f7f53c67cd1659d3be4986a2cb5c4711c6ccbf71
Christine <tinegulmatico@gmail.com>
2025-02-04 11:19:21 +0800

    fixed country code

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,27 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 // Login endpoint
 export const startLogin = async (phone: string) => {
-    const response = await axios.post(`${API_URL}/start-login/${phone}`);
+    const response = await axios.post(`/start-login/${phone}`);
     return response.data;
 };
 
 // Phone verification endpoint
 export const verifyLogin = async (phone: string, code: string) => {
-    const response = await axios.post(`${API_URL}/verify/${phone}/${code}`);
+    const response = await axios.post(`/verify/${phone}/${code}`);
     return response.data;
 };
 
 // Get list of groups by user endpoint
 export const getGroups = async (phone: string) => {
-    const response = await axios.get(`${API_URL}/get_groups/${phone}`);
+    const response = await axios.get(`/get_groups/${phone}`);
     return response.data;
 };
 
 // Leave selected groups endpoint
 export const leaveGroups = async (phone: string, group_ids: number[]) => {
-    const response = await axios.post(`${API_URL}/leave_groups/${phone}`, group_ids);
+    const response = await axios.post(`/leave_groups/${phone}`, group_ids);
     return response.data;
 };
--- src/util/countryCode.ts
+++ src/util/countryCode.ts
@@ -1,7 +1,7 @@
 export const countries = [
-    { code: "+63", name: "🇵🇭", regex: /^9\d{9}$/, example: "9123456789" },
-    { code: "+62", name: "🇮🇩", regex: /^8\d{9,12}$/, example: "81234567890" },
-    { code: "+65", name: "🇸🇬", regex: /^\d{8}$/, example: "81234567" },
-    { code: "+60", name: "🇲🇾", regex: /^[136789]\d{8,9}$/, example: "123456789" },
-    { code: "+66", name: "🇹🇭", regex: /^[689]\d{8}$/, example: "812345678" },
+    { code: "63", name: "🇵🇭", regex: /^9\d{9}$/, example: "9123456789" },
+    { code: "62", name: "🇮🇩", regex: /^8\d{9,12}$/, example: "81234567890" },
+    { code: "65", name: "🇸🇬", regex: /^\d{8}$/, example: "81234567" },
+    { code: "60", name: "🇲🇾", regex: /^[136789]\d{8,9}$/, example: "123456789" },
+    { code: "66", name: "🇹🇭", regex: /^[689]\d{8}$/, example: "812345678" },
 ];

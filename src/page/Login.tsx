93b216e0febfbf8659cac60e79052249e03a02e7
Christine <tinegulmatico@gmail.com>
2025-02-05 11:26:17 +0800

    adjusted

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -103,12 +103,20 @@ export const Login = () => {
                             <input type="text" placeholder="Enter phone number" value={phone.replace(country.code, "")} onChange={(e) => setPhone(country.code + e.target.value.trim())} className="input input-bordered w-full text-lg bg-base-100 rounded-lg" />
                         </div>
 
-                        <button onClick={handleLoginStart} className="btn btn-accent w-full mt-4 rounded-lg">Get Verification Code</button>
+                        <button onClick={handleLoginStart} className="btn btn-accent w-full mt-4 rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-telegram" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09" />
+                        </svg>
+                            Get Verification Code</button>
                     </div>
                 ) : (
                     <div className="mb-4 flex flex-col gap-2 justify-center items-center">
                         <input type="text" placeholder="Enter verification code" value={code} onChange={(e) => setCode(e.target.value)} className="input input-bordered w-full text-lg bg-base-100 rounded-lg" />
-                        <button onClick={handleVerifyCode} className="btn btn-accent w-full mt-4 rounded-lg">Login</button>
+                        <button onClick={handleVerifyCode} className="btn btn-accent w-full mt-4 rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-telegram" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09" />
+                        </svg>
+                            Login</button>
                     </div>
                 )}
             </div>
